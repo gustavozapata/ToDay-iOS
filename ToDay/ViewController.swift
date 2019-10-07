9006ebb81349834a85c7f5cb9afe8b8a88bea47b
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-06 21:22:59 -0700

    add SF font to project + start doing the main screen

--- ToDay/ViewController.swift
+++ ToDay/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nothingToDo: UILabel!
+    
+    @IBOutlet weak var taskNameInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        taskNameInput.isHidden = true
     }
 
-
+    @IBAction func addTaskButton(_ sender: Any) {
+        nothingToDo.isHidden = true
+        taskNameInput.isHidden = false
+    }
+    
 }
 
