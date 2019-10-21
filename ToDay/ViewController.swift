ffd55cde0f4e2f0bc5d9e38b1fb06e954a986b32
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-20 23:18:34 -0700

    added a label programmatically

--- ToDay/ViewController.swift
+++ ToDay/ViewController.swift
@@ -10,18 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nothingToDo: UILabel!
+    var tasks = [UILabel]()
+    var elY = 285
     
+    @IBOutlet weak var nothingToDo: UILabel!
     @IBOutlet weak var taskNameInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        taskNameInput.isHidden = true
+        if tasks.isEmpty {
+            taskNameInput.isHidden = true
+        }
+    }
+    
+    //PRESS ENTER
+    @IBAction func createNewTaskWithKey(_ sender: Any) {
+        if !taskNameInput.text!.isEmpty {
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 500, height: 30))
+            label.center = CGPoint(x: 290, y: elY)
+            elY += 50
+            label.font = UIFont.boldSystemFont(ofSize: 27.0)
+            
+            label.text = taskNameInput.text
+            tasks.append(label)
+            taskNameInput.text = ""
+            
+            for i in 0..<tasks.count {
+                self.view.addSubview(tasks[i])
+            }
+        }
     }
 
+    //+ BUTTON
     @IBAction func addTaskButton(_ sender: Any) {
-        nothingToDo.isHidden = true
-        taskNameInput.isHidden = false
+        if tasks.isEmpty {
+            nothingToDo.isHidden = true
+            taskNameInput.isHidden = false
+        }
     }
     
 }
